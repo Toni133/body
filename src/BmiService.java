1745c87a1361377b937e8d8599c4ad1c4ed4053c
Anton <toniq133@bk.ru>
2023-03-15 17:03:08 +0300

    first commit

---
+++ src/BmiService.java
@@ -0,0 +1,8 @@
+public class BmiService {
+
+    public int calculate(int weightInKg, double heightInMeters) {
+        return (int) (weightInKg / heightInMeters / heightInMeters);
+
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BmiService service = new BmiService();
+        double heightInMeters = 1.87;
+        int weightInKg = 98;
+        int Bmi = service.calculate(weightInKg, heightInMeters);
+        System.out.println(Bmi);
+        
     }
 }
